7c2b3c07ca600816816030b822bfc74854d3759d
hoki444 <hoki444@naver.com>
2014-11-21 22:49:34 +0900

    Modified Testsuite

--- schedcore/src/com/algy/schedcore/frontend/TestScene.java
+++ schedcore/src/com/algy/schedcore/frontend/TestScene.java
@@ -22,11 +22,57 @@ import com.badlogic.gdx.physics.bullet.collision.btBoxShape;
 import com.badlogic.gdx.physics.bullet.collision.btSphereShape;
 
 public class TestScene extends Scene {
-    private Model boxModel, ballModel;
+    @Override
+	public void reserveItem(Scene scene, ItemReservable coreProxy) {     
+		GameItem boardItem = new GameItem(), 
+                ballItem = new GameItem(),
+                lightItem = new GameItem();
+       boardItem.as(Transform.class).modify().setTranslation(0, 0, 0);
+       boardItem.add(BtRigidBodyComp
+                     .staticBody(new btBoxShape(new Vector3(2.f, .1f, 2.f)))
+                     .setFriction(0.7f)
+                     .setRestitution(0.8f));
+       boardItem.add(new ModelComp(boxModel));
+
+
+       ballItem.add(BtRigidBodyComp.dynamicBody(new btSphereShape(.4f), 1)
+                    .setAngularVelocity(new Vector3(0, 0, -10))
+                    .setLinearVelocity(new Vector3(0, 0, 0))
+                    .setRestitution(0.95f));
+       ballItem.add(new ModelComp(ballModel));
+       
+
+       lightItem.as(Transform.class).get().setTranslation(0, 2, 0);
+       lightItem.add(new DirectionalLightComp(new Vector3(0, -1f, 0.2f)).setColor(1.f, 1.f, 1.f, .2f));
+
+       for (float idx = 30; idx < 100; idx+=10) 
+           coreProxy.reserveItem(ballItem.duplicate(new Vector3(0, idx * 0.1f, 0)));
+       
+       coreProxy.reserveItem(boardItem);
+       coreProxy.reserveItem(lightItem);
+       
+       Done ();
+
+	}
+
+	@Override
+	public void endResourceInitialization(Scene scene) {
+        core.server(EnvServer.class).ambientLightColor.set(.2f, .2f, .2f, 1); 
+        core.server(BtPhysicsWorld.class).world.setGravity(new Vector3(0, -9.8f, 0));
+
+        core.server(CameraServer.class).setPosition(new Vector3(1, 5f, 1))
+                                       .lookAt(new Vector3(0, 0, 0))
+                                       .setUpVector(new Vector3(1, 0, 0)); 
+        Done ();
+	}
+
+	private Model boxModel, ballModel;
     private Texture tex;
+    private SpriteBatch sb;
+    
+    private int n = 0;
     @Override
     public void postRender() {
-        SpriteBatch sb = new SpriteBatch();
         sb.begin();
         sb.draw(tex, 0, 0, 100, 100);
         sb.end();
@@ -34,10 +80,8 @@ public class TestScene extends Scene {
 
     @Override
     public void firstPreparation() {
+    	sb  = new SpriteBatch();
         tex = new Texture("doge.jpg");
-        core.server(EnvServer.class).ambientLightColor.set(.2f, .2f, .2f, 1); 
-        core.server(BtPhysicsWorld.class).world.setGravity(new Vector3(0, -9.8f, 0));
-        
         boxModel = new ModelBuilder().createBox(4, .2f, 4, 
                 new Material(ColorAttribute.createDiffuse(0.1f, 0.1f, 0.1f, 0.1f),
                              ColorAttribute.createSpecular(.7f, .7f, .7f, 1f),
@@ -51,42 +95,9 @@ public class TestScene extends Scene {
     }
 
     @Override
-    public void prepare() {
-        core.server(CameraServer.class).setPosition(new Vector3(1, 5f, 1))
-                                       .lookAt(new Vector3(0, 0, 0))
-                                       .setUpVector(new Vector3(1, 0, 0)); 
-        GameItem boardItem = new GameItem(), 
-                 ballItem = new GameItem(),
-                 lightItem = new GameItem();
-        boardItem.as(Transform.class).modify().setTranslation(0, 0, 0);
-        boardItem.add(BtRigidBodyComp
-                      .staticBody(new btBoxShape(new Vector3(2.f, .1f, 2.f)))
-                      .setFriction(0.7f)
-                      .setRestitution(0.8f));
-        boardItem.add(new ModelComp(boxModel));
-
-
-        ballItem.add(BtRigidBodyComp.dynamicBody(new btSphereShape(.4f), 1)
-                     .setAngularVelocity(new Vector3(0, 0, -10))
-                     .setLinearVelocity(new Vector3(0, 0, 0))
-                     .setRestitution(0.95f));
-        ballItem.add(new ModelComp(ballModel));
-        
-
-        lightItem.as(Transform.class).get().setTranslation(0, 2, 0);
-        lightItem.add(new DirectionalLightComp(new Vector3(0, -1f, 0.2f)).setColor(1.f, 1.f, 1.f, .2f));
-        for (float idx = 30; idx < 100; idx+=10) 
-            core.addItem(ballItem.duplicate(new Vector3(0, idx * 0.1f, 0)));
-        
-        core.addItem(boardItem);
-        core.addItem(lightItem);
-
-        Done ();
-    }
-
-    @Override
     public void tearDown() {
         boxModel.dispose();
+        sb.dispose();
         Done ();
     }
 
