e0bcbf78f9fdf0bf95c190be91afd16b160a197c
hoki444 <hoki444@naver.com>
2014-11-30 15:49:32 +0900

    make the Sender UI skleton

--- PairPongSender/src/com/odk/pairpongsender/MainActivity.java
+++ PairPongSender/src/com/odk/pairpongsender/MainActivity.java
@@ -1,25 +1,35 @@
 package com.odk.pairpongsender;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.odk.pairpongsender.game.SenderFunction;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends Activity {
 	SenderFunction sfunction= new QPairSenderFunction(this);
-    
+	MainActivity myactivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+    	myactivity=this;
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        findViewById(R.id.sample_start_activity).setOnClickListener(this);
+        View vw=new MyView(this);
+		setContentView(vw);
     }
-    
-    @Override
-    public void onClick(View v) {
+    public void Startgame() {
         // bind QPair Service
     	sfunction.setpackage("com.odk.pairpong");
     	sfunction.startreceiver("PairPongBoardActivity");
@@ -27,5 +37,62 @@ public class MainActivity extends Activity implements View.OnClickListener {
     	
     	startActivity(intent);
     }
-
+    public void myDestroy(){
+		super.onDestroy();
+		System.exit(0);
+	}
+    
+    class MyView extends View{
+    	int mode;
+    	Point dsize;
+    	Display display;
+    	Resources res = getResources();
+    	Paint Pnt=new Paint();
+    	MainScreen mscreen;
+		public MyView(Context context) {
+			super(context);
+			mode=1;
+			dsize = new Point(0,0);
+			mscreen = new MainScreen();
+			display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+			display.getSize(dsize);
+			mHandler.sendEmptyMessage(0);
+		}
+		public void onDraw(Canvas canvas){
+			
+				canvas.drawColor(Color.LTGRAY);
+				if(mode==1)
+					mscreen.Draw(canvas, Pnt, dsize.x, dsize.y);
+				if(mode==3)
+					//option.Draw(canvas, Pnt, dsize.x, dsize.y);
+				if(mode==4){}
+					//highscore.Draw(canvas, Pnt, dsize.x, dsize.y, res);
+				
+		}
+		public boolean onTouchEvent(MotionEvent event)
+		{
+				if(event.getAction()==MotionEvent.ACTION_DOWN||event.getAction()==MotionEvent.ACTION_MOVE){
+					if(mode==1)
+						mode=mscreen.TouchEvent(event, dsize.x, dsize.y);
+					if(mode==3)
+						//mode=option.TouchEvent(event, dsize.x, dsize.y);
+					if(mode==4)
+						//mode=highscore.TouchEvent(event, dsize.x, dsize.y);
+					return true;
+				}
+			return false;
+		}
+		Handler mHandler = new Handler(){
+			public void handleMessage(Message msg){
+				invalidate();
+				if(mode==2){
+					mode=1;
+					myactivity.Startgame();
+				}
+				if(mode==5)
+					myactivity.myDestroy();
+				mHandler.sendEmptyMessageDelayed(0, 33);
+			}
+		};
+    }
 }
---
+++ PairPongSender/src/com/odk/pairpongsender/MainScreen.java
@@ -0,0 +1,48 @@
+package com.odk.pairpongsender;
+
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class MainScreen {
+	int textsize;
+	public void Draw(Canvas canvas, Paint pnt, int x, int y) {
+		textsize=Math.min(x/8,y/6);
+		pnt.setTextSize(textsize);
+		pnt.setColor(Color.BLACK);
+		canvas.drawText("Pair Pong", x/2-textsize*2.23f, y/5, pnt);
+		canvas.drawRect(x/3, y/3, x*2/3, y*9/20, pnt);
+		canvas.drawRect(x/3, y/2, x*2/3, y*37/60, pnt);
+		canvas.drawRect(x/3, y*2/3, x*2/3, y*47/60, pnt);
+		canvas.drawRect(x/3, y*5/6, x*2/3, y*19/20, pnt);
+		textsize=textsize/2;
+		pnt.setTextSize(textsize);
+		pnt.setColor(Color.WHITE);
+		canvas.drawText("Game Start", x/2-textsize*2.27f, y*49/120, pnt);
+		canvas.drawText("Option", x/2-textsize*1.5f, y*69/120, pnt);
+		canvas.drawText("High Score", x/2-textsize*2.27f, y*89/120, pnt);
+		canvas.drawText("Exit", x/2-textsize*1f, y*109/120, pnt);
+	}
+	public int TouchEvent(MotionEvent event, int x, int y) {
+		if(event.getX()>x/3&&event.getX()<x*2/3&&event.getAction()==MotionEvent.ACTION_DOWN){
+			if(event.getY()>y/3&&event.getY()<y*9/20){
+				return 2;
+			}
+			if(event.getY()>y/2&&event.getY()<y*37/60){
+				return 3;
+			}
+			if(event.getY()>y*2/3&&event.getY()<y*47/60){
+				return 4;
+			}
+			if(event.getY()>y*5/6&&event.getY()<y*19/20){
+				return 5;
+			}
+			return 1;
+		}
+		return 0;
+	}
+
+}
